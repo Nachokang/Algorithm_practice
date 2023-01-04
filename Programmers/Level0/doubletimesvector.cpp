833c37211088ba9b2ba3c8d39e5fc20d50bb0a5d
DaWoonKang <nachokang@ajou.ac.kr>
2023-01-04 23:01:01 +0900

    23.01.04

---
+++ Programmers/Level0/doubletimesvector.cpp
@@ -0,0 +1,28 @@
+﻿//
+// 2023.01.04
+// 프로그래머스 입문 - 배열 두 개 만들기
+// 강다운
+//
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer;
+    for (int i = 0; i < numbers.size(); i++) {
+        numbers[i] *= 2;
+        answer.push_back(numbers[i]);
+        // numbers 벡터의 원소를 두배한 값들을 answer 벡터에 넣어줌
+    }
+    return answer;
+}
+
+int main() {
+    vector<int> answer = solution({1, 2, 3, 4, 5});
+    for (int i = 0; i < answer.size(); i++)
+        cout << answer[i] << endl;
+}
---
+++ Programmers/Level0/fractionaddition.cpp
@@ -0,0 +1,45 @@
+﻿//
+// 2023.01.03
+// 프로그래머스 입문 - 분수의 덧셈
+// 강다운
+//
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int GCD(int num1, int num2) { // 최대공약수 (유클리드 호제법)
+    int gcd;
+    while (num2 != 0) {
+        gcd = num1 % num2;
+        num1 = num2;
+        num2 = gcd;
+    }
+    return num1;
+}
+
+int LCD(int num1, int num2) { // GCD를 이용해서 최소공배수
+    return num1 * num2 / GCD(num1, num2);
+}
+
+vector<int> solution(int denum1, int num1, int denum2, int num2) {
+    vector<int> answer;
+    int lcd = LCD(num1, num2); // 분자 두개의 최소공배수를 구한다 (통분하기 위해)
+    denum1 *= lcd / num1; 
+    denum2 *= lcd / num2;
+    // 분자 두개에 각각 분모의 최소공배수를 분모로 나눠준 수를 곱한다 (통분) 
+    int d = GCD((denum1+denum2), lcd);
+    // 기약분수로 만들어주기 위한 변수 d (통분된 분자 분모의 최대공약수를 구할수 있다면 구함)
+    answer.push_back((denum1+denum2) / d);
+    answer.push_back(lcd / d);
+    return answer;
+}
+
+int main() {
+    vector<int> answer = solution(9, 2, 1, 3);
+    for (int i = 0; i < answer.size(); i++)
+        cout << answer[i] << endl;
+
+}
