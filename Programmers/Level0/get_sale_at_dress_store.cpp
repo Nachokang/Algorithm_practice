757b482002d46303cb956a1062fbb98d615fb046
DaWoonKang <nachokang@ajou.ac.kr>
2023-01-12 22:50:18 +0900

    2023.01.12

---
+++ Programmers/Level0/get_sale_at_dress_store.cpp
@@ -0,0 +1,31 @@
+﻿//
+// 2023.01.12
+// 프로그래머스 0단계 - 옷가게 할인 받기
+// 강다운
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int price) {
+	int payment = price;
+	if (price >= 500000) {
+		payment = price * 0.8;
+	}
+	else if (price >= 300000) {
+		payment = price * 0.9;
+	}
+	else if (price >= 100000) {
+		payment = price * 0.95;
+	}
+	return payment;
+}
+
+int main() {
+	int answer = solution(580000);
+	cout << answer << endl;
+}
---
+++ Programmers/Level0/ice_americano.cpp
@@ -0,0 +1,28 @@
+﻿//
+// 2023.01.12
+// 프로그래머스 0단계 - 아이스 아메리카노
+// 강다운
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(int money) {
+	vector<int> answer;
+	int americano = money / 5500;
+	int leftover = money - 5500 * americano;
+	answer.push_back(americano);
+	answer.push_back(leftover);
+	return answer;
+}
+
+int main() {
+	vector<int> answer = solution(5500);
+	for (int i = 0; i < answer.size(); i++) {
+		cout << answer[i] << endl;
+	}
+}
