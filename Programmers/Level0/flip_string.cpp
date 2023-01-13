f5d44fa7ab763ecb849435d7fc0a77e4d4c6f358
DaWoonKang <nachokang@ajou.ac.kr>
2023-01-13 13:46:10 +0900

    2023-01-13

---
+++ Programmers/Level0/flip_string.cpp
@@ -0,0 +1,26 @@
+﻿//
+// 2023.01.13
+// 프로그래머스 0단계 - 문자열 뒤집기
+// 강다운
+//
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+string solution(string my_string) {
+    string answer;
+    reverse(my_string.begin(), my_string.end());
+    for (int i = 0; i < my_string.size(); i++) {
+        answer.push_back(my_string[i]);
+    }
+    return answer;
+}
+
+int main() {
+    string answer = solution("jaron");
+    cout << answer << endl;
+}
