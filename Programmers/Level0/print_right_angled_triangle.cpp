b8a4dc1b195b860f72e96903d77a944a95a59862
DaWoonKang <nachokang@ajou.ac.kr>
2023-01-13 13:59:53 +0900

    2023.01.13

---
+++ Programmers/Level0/print_right_angled_triangle.cpp
@@ -0,0 +1,28 @@
+﻿//
+// 2023.01.13
+// 프로그래머스 0단계 - 직각삼각형 출력하기
+// 강다운
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void triangle(int n) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j <= i; j++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+	cout << "\n";
+}
+
+int main(void) {
+	int n;
+	cin >> n;
+	triangle(n);
+	return 0;
+}
