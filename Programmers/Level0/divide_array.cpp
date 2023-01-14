4ea6794d7fdeeb2bcf50580f1f58e0589ffa0bb7
DaWoonKang <nachokang@ajou.ac.kr>
2023-01-14 20:15:21 +0900

    Q#120833 2023.01.14

---
+++ Programmers/Level0/divide_array.cpp
@@ -0,0 +1,25 @@
+﻿//
+// 프로그래머스 0단계 - 배열 자르기 (120833)
+// 2023.01.14
+// 강다운
+//
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers, int num1, int num2) {
+	vector<int> sliced_vec(numbers.begin() + num1, numbers.begin() + num2 + 1);
+	return sliced_vec;
+}
+
+int main() {
+	vector<int> answer = solution({ 1,2,3,4,5 }, 1, 3);
+	for (int i = 0; i < answer.size(); i++) {
+		cout << answer[i] << " ";
+	}
+}
