5754038237cea63417dc645eeabaaf7b722c2d63
DaWoonKang <nachokang@ajou.ac.kr>
2023-01-16 13:48:24 +0900

    Q#120841 2023.01.16

---
+++ Programmers/Level0/get_where_does_point_exists.cpp
@@ -0,0 +1,36 @@
+﻿//
+// 2023.01.16
+// 프로그래머스 0단계 - 점의 위치 구하기 (120841)
+// code by 강다운
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int solution(vector<int> dot) {
+	int quadrant = 0;
+	if (dot[0] > 0) {
+		if (dot[1] > 0)
+			quadrant = 1;
+		else
+			quadrant = 4;
+	}
+	
+	if (dot[0] < 0) {
+		if (dot[1] > 0)
+			quadrant = 2;
+		else
+			quadrant = 3;
+	}
+
+	return quadrant;
+}
+
+int main() {
+	int answer = solution({ 2,4 });
+	cout << answer;
+}
