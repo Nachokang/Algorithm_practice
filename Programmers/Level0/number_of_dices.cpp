304a2b5d11cb3bf1c86886b2b114c70b2786dea2
DaWoonKang <nachokang@ajou.ac.kr>
2023-01-18 12:46:24 +0900

    Q#120845 2023.01.08

---
+++ Programmers/Level0/number_of_dices.cpp
@@ -0,0 +1,27 @@
+﻿//
+// 2023.01.18
+// 프로그래머스 0단계 - 주사위의 개수 (120845)
+// code by 강다운
+//
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int solution(vector<int> box, int n) {
+	int dice = 1;
+	for (int i = 0; i < box.size(); i++) {
+		box[i] /= n;
+		dice *= box[i];
+	}
+	return dice;
+}
+
+int main() {
+	int answer = solution({ 10,8,6 }, 3);
+	cout << answer;
+}
