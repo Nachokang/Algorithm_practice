9e5df6d9eaebcf8b8f66bd465378b2474eb696b1
DaWoonKang <nachokang@ajou.ac.kr>
2023-02-17 16:54:25 +0900

    Q#120846 2023.02.17

---
+++ Programmers/Level0/find_composite_number.cpp
@@ -0,0 +1,34 @@
+﻿//
+// 2023.02.17
+// 프로그래머스 0단계 - 합성수 찾기 (120846)
+// code by 강다운
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int solution(int n) {
+	vector<int> primenum;
+	int compositenum = 0;
+	int cnt;
+	for (int i = 2; i <= n; i++) {
+		cnt = 0;
+		for (int j = 2; j <= n; j++) {
+			if (i % j == 0)
+				cnt++;
+		}
+		if (cnt == 1)
+			primenum.push_back(i);
+	}
+	compositenum = n - primenum.size()-1; // 1은 소수가 아니므로 -1을 해준다
+	return compositenum;
+}
+
+int main() {
+	int answer = solution(10);
+	cout << answer;
+}
