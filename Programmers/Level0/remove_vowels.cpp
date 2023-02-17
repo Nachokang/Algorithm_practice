f99c8e7372a991f23356fc089ea418077a67eb06
DaWoonKang <nachokang@ajou.ac.kr>
2023-02-17 15:29:45 +0900

    Q#120849 2023.02.17

---
+++ Programmers/Level0/remove_vowels.cpp
@@ -0,0 +1,27 @@
+﻿//
+// 2023.02.17
+// 프로그래머스 0단계 - 모음 제거 (120849)
+// code by 강다운
+//
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+string solution(string my_string) {
+	vector<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+
+	for (int i = 0; i < 5; i++) {
+		my_string.erase(remove(my_string.begin(), my_string.end(), vowels[i]), my_string.end());
+	}
+
+	return my_string;
+}
+
+int main() {
+	string answer = solution("nice to meet you");
+	cout << answer << endl;
+}
