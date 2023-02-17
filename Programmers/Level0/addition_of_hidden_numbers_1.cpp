53bbb1f3442e95cfca1d6c83a5132f9c13ce7994
DaWoonKang <nachokang@ajou.ac.kr>
2023-02-17 17:23:27 +0900

    Q#120851 23.02.17

---
+++ Programmers/Level0/addition_of_hidden_numbers_1.cpp
@@ -0,0 +1,33 @@
+﻿//
+// 2023.02.17
+// 프로그래머스 0단계 - 숨어있는 숫자의 덧셈(1) (120851)
+// code by 강다운
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int solution(string my_string) {
+	int sum = 0;
+	int cnt = 0;
+	vector<int> onlynum;
+	for (int i = 0; i < my_string.size(); i++) {
+		if (my_string[i] > 47 && my_string[i] < 58) {
+			cnt = my_string[i] - 48;
+			onlynum.push_back(cnt);
+		}
+	}
+	for (int j = 0; j < onlynum.size(); j++) {
+		sum += onlynum[j];
+	}
+	return sum;
+}
+
+int main() {
+	int answer = solution("aAb1B2cC34oOp");
+	cout << answer;
+}
