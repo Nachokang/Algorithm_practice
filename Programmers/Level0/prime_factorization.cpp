9b688857dc1ac67cfb0eee1ae82e4c4aa24a746b
DaWoonKang <nachokang@ajou.ac.kr>
2023-03-21 13:55:27 +0900

    Q#120852 23.03.21

---
+++ Programmers/Level0/prime_factorization.cpp
@@ -0,0 +1,39 @@
+﻿//
+// 프로그래머스 0단계 - 소인수분해 (120852)
+// 2023.03.21
+// code by 강다운
+//
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<int> solution(int n) {
+	vector<int> primenum;
+	int cnt = 0;
+	for (int i = 2; i <= n; ++i) {
+		while (n % i == 0) {
+			n /= i;
+			primenum.push_back(i);
+		}
+	}
+	if (n != 1) {
+		primenum.push_back(n);
+	}
+
+	sort(primenum.begin(), primenum.end());
+	primenum.erase(unique(primenum.begin(), primenum.end()), primenum.end());
+	// vector 안에 중복 원소 제거
+
+	return primenum;
+}
+
+int main() {
+	vector<int> answer = solution(420);
+	for (int i = 0; i < answer.size(); i++) {
+		cout << answer[i] << " ";
+	}
+}
