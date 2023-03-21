98a5008268ba98dd54ae24a3499d51e8decbdf62
DaWoonKang <nachokang@ajou.ac.kr>
2023-03-21 14:13:33 +0900

    Q#120854 23.03.21

---
+++ Programmers/Level0/size_of_elements_in_array.cpp
@@ -0,0 +1,28 @@
+﻿//
+// 프로그래머스 0단계 - 배열 원소의 길이 (120854)
+// 20203.03.21
+// code by 강다운
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+vector<int> solution(vector<string> strlist) {
+	vector<int> element_len;
+	for (auto i : strlist) {
+		element_len.push_back(i.size());
+	}
+
+	return element_len;
+}
+
+int main() {
+	vector<int> answer = solution({ "We", "are", "the", "world!" });
+	for (auto i : answer) {
+		cout << i << " ";
+	}
+}
