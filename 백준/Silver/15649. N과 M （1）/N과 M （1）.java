016665e42ee9ccbfc7081b57bb60379f05acfc62
DaWoon Kang <89457065+Nachokang@users.noreply.github.com>
2024-01-23 14:10:19 +0900

    [Silver III] Title: N과 M (1), Time: 2088 ms, Memory: 52056 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] arr;
+    static boolean[] isUsed;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[10];
+        isUsed = new boolean[10];
+
+        printArr(0);
+
+    }
+
+    static void printArr(int k) {
+
+        if(k==M) {
+            for(int i=0; i<M; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i = 1; i<=N; i++) {
+            if(!isUsed[i]) {
+                arr[k] = i;
+                isUsed[i] = true;
+                printArr(k+1);
+                isUsed[i] = false;
+            }
+        }
+    }
+}
