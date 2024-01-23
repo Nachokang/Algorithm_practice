76a8c7f991fafa64249644f98ad397f83f9b11c7
DaWoon Kang <89457065+Nachokang@users.noreply.github.com>
2024-01-23 14:11:54 +0900

    [Silver III] Title: N과 M (3), Time: 592 ms, Memory: 158456 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] arr;
+    static boolean[] isUsed;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[10];
+        isUsed = new boolean[10];
+
+        printArr(0, 1);
+        bw.close();
+
+    }
+
+    static void printArr(int k, int start) throws IOException {
+
+        if(k==M) {
+            for(int i=0; i<M; i++) {
+                bw.write(arr[i] + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+
+        for(int i = 1; i<=N; i++) {
+            if(!isUsed[i]) {
+                arr[k] = i;
+                printArr(k+1, i);
+                isUsed[i] = false;
+            }
+        }
+    }
+}
