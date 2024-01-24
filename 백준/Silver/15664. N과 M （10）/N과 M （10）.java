35cb913ed16e8c0320b32b1fc54b8136c608a0cd
DaWoon Kang <89457065+Nachokang@users.noreply.github.com>
2024-01-24 15:56:29 +0900

    [Silver II] Title: N과 M (10), Time: 144 ms, Memory: 15780 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] arr;
+    static boolean[] isUsed;
+    static int[] numbers;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        numbers = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(numbers);
+
+        arr = new int[N];
+        isUsed = new boolean[N];
+
+        printArr(0, 0);
+        bw.close();
+
+    }
+
+    static void printArr(int k, int start) throws IOException {
+
+        if(k==M) {
+            for(int i=0; i<M; i++) {
+                bw.write(arr[i] + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+
+        int before = 0;
+        for(int i = start; i<N; i++) {
+            if(!isUsed[i]) {
+                if(before != numbers[i]) {
+                    arr[k] = numbers[i];
+                    isUsed[i] = true;
+                    before = numbers[i];
+                    printArr(k+1, i+1);
+                    isUsed[i] = false;
+                }
+            }
+        }
+    }
+}
