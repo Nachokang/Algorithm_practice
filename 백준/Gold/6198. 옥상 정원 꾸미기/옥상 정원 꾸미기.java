9be6da7c22d205c4077c0672e27fa00acaf0cef6
DaWoon Kang <89457065+Nachokang@users.noreply.github.com>
2024-04-08 19:09:01 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 360 ms, Memory: 27620 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
@@ -3,46 +3,31 @@ import java.util.*;
 
 public class Main {
     static int N;
-    static int[] height;
-    static int[] count;
-    static long buildings;
+    static long answer;
+    static Stack<Integer> s;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
+        s = new Stack<>();
 
-        height = new int[N+1];
         for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
-            height[i] = Integer.parseInt(st.nextToken());
-        }
+            int height = Integer.parseInt(st.nextToken());
 
-        int temp = height[0];
-        int temp2 = 0;
-        count = new int[N+1];
-
-        for(int i=0; i<N+1; i++) {
-            if(i==N) {
-                break;
-            }
-            temp2 = i+1;
-            while(true) {
-                if(height[temp2]<temp && height[temp2]!=0) {
-                    count[i] += 1;
-                    temp2++;
+            while(!s.isEmpty()) {
+                if(s.peek() <= height) {
+                    s.pop();
                 }
                 else {
-                    temp = height[i+1];
                     break;
                 }
             }
+            answer += s.size();
+            s.push(height);
         }
-
-        for(int i : count) {
-            buildings += i;
-        }
-
-        System.out.print(buildings);
+        
+        System.out.print(answer);
     }
 }
